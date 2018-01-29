c2c0491cc38c4ecb85ecf348334d75430a7ea8a7
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2018-01-29 13:30:50 -1000

    model matrix with translate and scale

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+var canvas = document.createElement('canvas')
+document.body.appendChild(onresize(canvas))
+window.addEventListener('resize', onresize)
+function onresize () {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  canvas.style.position = 'absolute'
+  canvas.style.width = '100%'
+  canvas.style.height = '100%'
+  canvas.style.top = '0px'
+  canvas.style.left = '0px'
+  return canvas
+}
+
 var regl = require('regl')({
+  canvas: canvas,
   extensions: [
     'oes_standard_derivatives', 'oes_element_index_uint',
     'oes_texture_float'
@@ -11,22 +26,21 @@ var app = {
 }
 require('./lib/state/frame.js')(app.state, app.emitter)
 require('./lib/state/camera.js')(app.state, app.emitter)
-require('./lib/state/meshes.js')(app.state, app.emitter)
+require('./lib/state/geometry.js')(app.state, app.emitter)
 
 app.emitter.emit('draw-module', require('./lib/draw/solid.js'))
 app.emitter.emit('draw-module', require('./lib/draw/select-box.js'))
 
+canvas.addEventListener('mousedown', onmouse)
+canvas.addEventListener('mouseup', onmouse)
+canvas.addEventListener('mouseclick', onmouse)
+canvas.addEventListener('mousemove', onmouse)
+
+function onmouse (ev) {
+  app.emitter.emit('mouse', ev)
+}
+
 var mat4 = require('gl-mat4')
-app.emitter.emit('add-mesh', 'camera', Object.assign({
-  model: function (m) {
-    mat4.identity(m)
-    mat4.rotateY(m,m,performance.now()*0.001)
-    return m
-  }
-}, require('./lib/mesh/cool.json')))
+app.emitter.emit('add-mesh', 'camera', require('./lib/mesh/cool.json'))
 
 app.emitter.emit('regl', regl)
-
-app.emitter.on('post-frame', function () {
-  app.emitter.emit('frame')
-})
